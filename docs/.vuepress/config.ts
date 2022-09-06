ddedbd602136f0fbd21179526edad92bc65d641d
Marvel <670953242@qq.com>
2022-09-06 23:54:56 +0800

    更新内容+action自动部署

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -184,11 +184,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       bgTimeColorArray: ['transparent', 'rgba(255, 148, 48, .2)', 'rgba(0, 0, 0, .3)', 'rgba(0, 0, 0, .5)'],   // 第一个是白天的颜色（默认原图），第二个是黄昏的颜色，第三个是晚上的颜色，第四个是深夜的颜色。bgTimeColor 为 true 生效。提示：如果不想要这个效果，但是又想要时间窗口提示效果，则改为 ['transparent', 'transparent', 'transparent', 'transparent']
       descFade: true,   // 是否开启图片中间描述的淡入效果，默认为 false
       desc: [ // 多个描述，如果填写则覆盖 config.js 的 description，不填写默认读取 config.js 的 description，descFadeIn 为 true 生效
-        "欢迎光临本站，我是一名致力于抢计算机饭碗的自动化专业在读研究生！  ——Marvel", 
         "天下事有难易乎?为之，则难者亦易矣；不为，则易者亦难矣。  ——为学", 
         "但凡不能杀死你的，最终都会使你更强大。 ——尼采",
         "每一个不曾起舞的日子，都是对生命的辜负。 ——尼采",
-        "与怪物战斗的人，应当小心自己不要成为怪物。当你远远凝视深渊时，深渊也在凝视你。 ——尼采"
+        "与怪物战斗的人，应当小心自己不要成为怪物。当你远远凝视深渊时，深渊也在凝视你。 ——尼采",
+        "欢迎光临本站，我是一名致力于抢计算机饭碗的自动化专业在读研究生！  ——Marvel",
       ],  
       descFontSize: '1.4rem',   // desc 的字体大小，默认 1.4rem。提示：原主题是 1.1rem
       descFadeInTime: 200,  // 描述的淡入效果持续时间，descFade 为 true 生效，默认 200 毫秒
